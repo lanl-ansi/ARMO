03d52100e80d65f3c1ec4b089fc7764bdd8a1121
Hassan <hlh@Hassans-iMac.local>
2022-02-23 18:45:16 -0700

    cleaning up ARMO

--- cmake/ExternalGravity.cmake
+++ cmake/ExternalGravity.cmake
@@ -1,30 +1,58 @@
 # Create download URL derived from version number.
-set(Gravity_HOME https://github.com/coin-or/Gravity/archive)
-set(Gravity_DOWNLOAD_URL ${Gravity_HOME}/matsup.tar.gz)
-unset(Gravity_HOME)
+set(GRAVITY_DOWNLOAD_URL https://github.com/coin-or/Gravity.git)
 
-# Download and build the Gravity library and add its properties to the third party arguments.
-set(Gravity_ROOT_DIR ${THIRDPARTY_INSTALL_PATH}/Install/gravity CACHE INTERNAL "")
+# Download and build the GRAVITY library and add its properties to the third party arguments.
+set(GRAVITY_ROOT_DIR ${THIRDPARTY_INSTALL_PATH}/Install/Gravity CACHE INTERNAL "")
+
+if(WIN32)
+# On Windows, download the Ipopt dlls from https://github.com/IDAES and the Gravity dlls from https://github.com/coin-or/Gravity
 ExternalProject_Add(gravity
     DOWNLOAD_DIR ${THIRDPARTY_INSTALL_PATH}
-    DOWNLOAD_COMMAND export HTTP_PROXY=$ENV{HTTP_PROXY} && export HTTPS_PROXY=$ENV{HTTPS_PROXY} && export http_proxy=$ENV{HTTP_PROXY} && curl -k -L ${Gravity_DOWNLOAD_URL} -o Gravity-matsup.tar.gz && tar -xvf Gravity-matsup.tar.gz && rm -fr ./Install/gravity && mv Gravity-matsup gravity && mv gravity ./Install && cd ./Install/gravity && cmake . && make -j24 gravity
-    URL ${Gravity_DOWNLOAD_URL}
+    DOWNLOAD_COMMAND curl -k -L https://github.com/IDAES/idaes-ext/releases/download/2.4.1/idaes-solvers-windows-64.tar.gz -o idaes-solvers-windows-64.tar.gz && tar -xvzf idaes-solvers-windows-64.tar.gz -C ${GRAVITY_ROOT_DIR}
+    URL ${GRAVITY_DOWNLOAD_URL}
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${GRAVITY_ROOT_DIR}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
 )
+set(IPOPT_ROOT_DIR ${THIRDPARTY_INSTALL_PATH}/Install/Gravity/thirdparty/Ipopt CACHE INTERNAL "")
+add_custom_command(
+  TARGET gravity POST_BUILD
+  COMMAND ${CMAKE_COMMAND} -E copy 
+  ${GRAVITY_ROOT_DIR}/libipopt-3.dll ${PROJECT_SOURCE_DIR}/bin/Release/libipopt-3.dll)
 
-list(APPEND GLOBAL_THIRDPARTY_LIB_ARGS "-DGravity_ROOT_DIR:PATH=${Gravity_ROOT_DIR}")
-set(GRAVITY_INCLUDE_DIRS ${Gravity_ROOT_DIR}/include)
-include_directories(${GRAVITY_INCLUDE_DIRS})
-find_library(GRAVITY_LIBRARY1
-        libgravity.a
-        HINTS /usr/local/lib
-        HINTS ${Gravity_ROOT_DIR}/lib
+add_custom_command(
+TARGET gravity POST_BUILD
+COMMAND ${CMAKE_COMMAND} -E copy 
+${GRAVITY_ROOT_DIR}/libblas.dll ${PROJECT_SOURCE_DIR}/bin/Release/libblas.dll)
+
+add_custom_command(
+TARGET gravity POST_BUILD
+COMMAND ${CMAKE_COMMAND} -E copy 
+${GRAVITY_ROOT_DIR}/liblapack.dll ${PROJECT_SOURCE_DIR}/bin/Release/liblapack.dll)
+
+add_custom_command(
+TARGET gravity POST_BUILD
+COMMAND ${CMAKE_COMMAND} -E copy 
+${GRAVITY_ROOT_DIR}/libgfortran-5.dll ${PROJECT_SOURCE_DIR}/bin/Release/libgfortran-5.dll)
+
+else()
+ExternalProject_Add(gravity
+    DOWNLOAD_DIR ${THIRDPARTY_INSTALL_PATH}
+    DOWNLOAD_COMMAND export HTTPS_PROXY=$ENV{HTTPS_PROXY} && git clone -b ACOPF --single-branch ${GRAVITY_DOWNLOAD_URL} && rm -fr ./Install/Gravity && mv Gravity ./Install/Gravity && cd ./Install/Gravity && mkdir build && cd build && cmake -DMP=OFF -DCMAKE_CXX_FLAGS="-Wno-non-pod-varargs" .. && make gravity
+    URL ${MP_DOWNLOAD_URL}
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${MP_ROOT_DIR}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
 )
+endif()
 
-
-set(GRAVITY_LIBRARIES  ${GRAVITY_LIBRARY1})
-set(LIBS ${LIBS} ${GRAVITY_LIBRARIES})
-unset(Gravity_DOWNLOAD_URL)
-unset(Gravity_ROOT)
+list(APPEND GLOBAL_THIRDPARTY_LIB_ARGS "-DGRAVITY_ROOT_DIR:PATH=${GRAVITY_ROOT_DIR}")
+set(GRAVITY_INCLUDE_DIRS ${THIRDPARTY_INSTALL_PATH}/Install/Gravity/include)
+include_directories(${GRAVITY_INCLUDE_DIRS})
+find_library(GRAVITY_LIBRARY
+        libgravity.a
+        HINTS ${GRAVITY_ROOT_DIR}/build/lib
+)
+set(LIBS ${LIBS} ${GRAVITY_LIBRARY})
--- cmake/FindGRAVITY.cmake
+++ cmake/FindGRAVITY.cmake
@@ -1,34 +1,56 @@
-# Search supplied hint directories first if supplied.
 set(GRAVITY_ROOT_DIR "$ENV{GRAVITY_ROOT_DIR}" CACHE PATH "GRAVITY root directory.")
-if(GRAVITY_ROOT_DIR)
- message("Looking for GRAVITY in ${GRAVITY_ROOT_DIR}")
-else(GRAVITY_ROOT_DIR)
- message("GRAVITY_ROOT_DIR not provided.")
-endif(GRAVITY_ROOT_DIR)
+message("Looking for GRAVITY in ${GRAVITY_ROOT_DIR}")
 
-find_path(GRAVITY_INCLUDE_DIR
-  NAMES solver.h
-  HINTS ${GRAVITY_ROOT_DIR}/include/gravity
-  HINTS /usr/local/include
-  HINTS /usr/include  
-  HINTS ${THIRDPARTY_INSTALL_PATH}/Gravity/include/gravity
-)
-message("GRAVITY_INCLUDE_DIR  = ${GRAVITY_INCLUDE_DIR}.")
-find_library(GRAVITY_LIBRARY 
-	libgravity.a
-	HINTS ${GRAVITY_ROOT_DIR}/lib
-  HINTS ${GRAVITY_ROOT_DIR}/lib/Release
-	HINTS /usr/local/lib
-)
-message("GRAVITY_LIBRARY  = ${GRAVITY_LIBRARY}.")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GRAVITY DEFAULT_MSG GRAVITY_LIBRARY GRAVITY_INCLUDE_DIR)
 
-# Set standard CMake FindPackage variables if found.
-if (GRAVITY_FOUND)
-  set(GRAVITY_INCLUDE_DIRS ${GRAVITY_INCLUDE_DIR}/..)
-  set(GRAVITY_LIBRARIES ${GRAVITY_LIBRARY})
+find_path(GRAVITY_INCLUDE_DIR
+	NAMES model.h 
+	HINTS ${GRAVITY_ROOT_DIR}/include/gravity
+	HINTS ${PROJECT_SOURCE_DIR}/thirdparty/Gravity/include/gravity
+)
+
+
+if(WIN32)
+
+find_library(GRAVITY_LIBRARY 
+	libgravity.lib
+	HINTS /usr/local/lib
+	HINTS ${GRAVITY_ROOT_DIR}/lib
+	HINTS ${PROJECT_SOURCE_DIR}/thirdparty/Gravity/lib
+)
+
+elseif(APPLE)
+find_library(GRAVITY_LIBRARY 
+	libgravity.dylib
+	HINTS /usr/local/lib
+	HINTS ${GRAVITY_ROOT_DIR}/lib
+	HINTS ${PROJECT_SOURCE_DIR}/third_party/CoinGRAVITY/build/lib
+)
+
+elseif(UNIX)
+find_library(GRAVITY_LIBRARY 
+	libgravity.so
+	HINTS /usr/local/lib
+	HINTS ${GRAVITY_ROOT_DIR}/lib
+	HINTS ${PROJECT_SOURCE_DIR}/third_party/CoinGRAVITY/build/lib
+)
+endif(WIN32)
+
+find_package_handle_standard_args(Gravity DEFAULT_MSG GRAVITY_LIBRARY GRAVITY_INCLUDE_DIR)
+
+if(GRAVITY_FOUND)
+	message("—- Found GRAVITY include dir under ${GRAVITY_INCLUDE_DIR}")
+	message("—- Found GRAVITY lib at ${GRAVITY_LIBRARY}")
+    set(GRAVITY_INCLUDE_DIRS ${GRAVITY_INCLUDE_DIR})
+    set(GRAVITY_LIBRARIES ${GRAVITY_LIBRARY})
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        set(GRAVITY_LIBRARIES "${GRAVITY_LIBRARIES};m;pthread")
+    endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 else (GRAVITY_FOUND)
  message("Cannot find GRAVITY, will try pulling it from github.")
-endif (GRAVITY_FOUND)
+endif(GRAVITY_FOUND)
+
+mark_as_advanced(GRAVITY_LIBRARY GRAVITY_INCLUDE_DIR)
+
+
